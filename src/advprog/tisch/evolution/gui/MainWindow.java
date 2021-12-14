f3bed5f53e1e51031243257bbd9fb6cb11ad5b2b
lmeister <leonmeister97@gmail.com>
2021-12-14 14:40:59 +0100

    Add spinnermodels and initial values

--- src/advprog/tisch/evolution/gui/MainWindow.java
+++ src/advprog/tisch/evolution/gui/MainWindow.java
@@ -3,6 +3,8 @@ package advprog.tisch.evolution.gui;
 import com.sun.tools.javac.Main;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainWindow extends JFrame {
     private JTextArea outputTextArea;
@@ -27,10 +29,46 @@ public class MainWindow extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+
+        // This will run the optimizer
+        runButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                // Get all configuration data from the spinners
+                // Call the optimizer
+
+            }
+        });
+
+
     }
 
     public static void main(String[] args) {
         JFrame frame = new MainWindow("Genetically Evolving the Perfect Table");
         frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        SpinnerNumberModel lengthDeviation = new SpinnerNumberModel(0.15, 0.0, 1.00, 0.01);
+        this.lengthDeviationSpinner = new JSpinner();
+        this.lengthDeviationSpinner.setModel(lengthDeviation);
+
+        SpinnerNumberModel mutationRate = new SpinnerNumberModel(0.1, 0.0, 1.00, 0.01);
+        this.mutationRateSpinner = new JSpinner();
+        this.mutationRateSpinner.setModel(mutationRate);
+
+        SpinnerNumberModel tournamentSize = new SpinnerNumberModel(0.2, 0.0, 1.00, 0.01);
+        this.tournamentSizeSpinner = new JSpinner();
+        this.tournamentSizeSpinner.setModel(tournamentSize);
+
+        SpinnerNumberModel generationSize = new SpinnerNumberModel(10, 10, 500, 1);
+        this.generationSizeSpinner = new JSpinner();
+        this.generationSizeSpinner.setModel(generationSize);
+
+        SpinnerNumberModel maxGenerations = new SpinnerNumberModel(5, 1, 500, 1);
+        this.maximumGenerationsSpinner = new JSpinner();
+        this.maximumGenerationsSpinner.setModel(maxGenerations);
+
     }
 }
